4f1b51bdc9bb3757530f1624ec5ef311c3b88b75
Subhajit <022subha@gmail.com>
2023-06-08 11:52:16 +0530

    footer and about added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <About />
       <Footer />
     </BrowserRouter>
   );
