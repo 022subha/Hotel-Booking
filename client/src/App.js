0eb4563eb5f678fb46df745c4de3d25840b5d2cf
Golu kumar <anubhavanandgolu@gmail.com>
2023-06-08 11:53:04 +0530

    about-and-footer-added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-
-
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-
+import About from "./components/About/About";
 function App() {
   return (
     <BrowserRouter>
+      <About/>
       <Footer />
     </BrowserRouter>
   );
