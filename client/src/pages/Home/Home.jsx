e506554c44d1fe7483c6c2627e1e60f88ea79572
Subhajit <022subha@gmail.com>
2023-06-08 15:51:14 +0530

    Searchbar Stared

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "../../components/About/About";
 import Banner from "../../components/Banner/Banner";
+import Searchbar from "../../components/Searchbar/Searchbar";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Searchbar />
       <About />
     </>
   );
