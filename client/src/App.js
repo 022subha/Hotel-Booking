58c1e2abce826b0d2b767572ed61b524bf1ac24b
Golu kumar <anubhavanandgolu@gmail.com>
2023-06-09 12:41:12 +0530

    footer-removed

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/rooms" element={<Rooms/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -11,7 +11,6 @@ export default function Home() {
       <Searchbar />
       <Testimonials />
       <About />
-      <Footer/>
     </>
   );
 }
