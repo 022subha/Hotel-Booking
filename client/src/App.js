b268bf05c60ff48ba9c3dea25efae52f09097b21
Golu kumar <anubhavanandgolu@gmail.com>
2023-06-09 12:38:38 +0530

    rooms-middle

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
-
+import Rooms from "./pages/Rooms/Rooms";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms/>}/>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import About from "../../components/About/About";
 import Banner from "../../components/Banner/Banner";
 import Testimonials from "../../components/Testimonials/Testimonials";
+import Footer from "../../components/Footer/Footer";
 export default function Home() {
   return (
     <>
     <Banner/>
     <Testimonials/>
       <About />
+      <Footer/>
     </>
   );
 }
