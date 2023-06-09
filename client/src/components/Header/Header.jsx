4d66570c0f3d654c8bb98f8f616d82c6fe09fc93
Bijay kumar sah <vijaykumarsah293@gmail.com>
2023-06-09 12:35:50 +0530

    header started

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <>
+            <div className="header-area">
+                <div className="top-header-area"></div>
+                <div className="main-header-area"></div>
+            </div>
+
+        </>
+    );
+};
+
+export default Header;
