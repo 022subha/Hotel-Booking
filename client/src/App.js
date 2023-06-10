dd46c7ee26f2107ec8ac3568f700ffc4fe76880f
Subhajit <022subha@gmail.com>
2023-06-10 11:56:25 +0530

    single-room 10% done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import SingleRoom from "./pages/SingleRoom/SingleRoom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/single-room" element={<SingleRoom />} />
       </Routes>
       <Footer />
     </BrowserRouter>
   );
 }
- export default App;
- 
+export default App;
