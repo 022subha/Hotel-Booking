632775cfb9756a3ab3776bb50e3208d27957bd79
Golu kumar <anubhavanandgolu@gmail.com>
2023-06-18 10:13:17 +0530

    login-function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,37 @@
-export const register = async (req, res) => {
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    console.log(req.body);
+    let existinguser;
+    existinguser = await User.findOne({ email });
+    if (!existinguser) {
+      return res
+        .status(201)
+        .json({ status: false, message: "User Not Available!!" });
+    }
+    const isMatch = bcrypt.compare(password, existinguser.password);
+    if (!isMatch) {
+      return res
+        .status(201)
+        .json({ status: false, message: "Error in Password!!" });
+    }
+    const token = jwt.sign(
+      {
+        id: existinguser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    return res
+      .status(200)
+      .json({ status: true, message: "Login Successfully!!!", token });
   } catch (error) {
     console.log(error);
     return res
       .status(500)
-      .json({ status: false, message: "Interna Server Error !!" });
+      .json({ status: false, message: "Internal Server Error" });
   }
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { login } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
 
+router.route("/login").post(login);
 export default router;
