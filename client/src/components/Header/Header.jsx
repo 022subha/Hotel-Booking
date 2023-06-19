332bc5b79fd0a1bd0e97808029e6e93e51bb3a43
Bijay kumar sah <vijaykumarsah293@gmail.com>
2023-06-19 11:22:42 +0530

    header made responsive

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
                 <div className="top-header-area">
                     <div className="phemail">
                         <div className="phone-no">
-                        <ion-icon name="call"></ion-icon> <div>+91-98655 41789</div>
+                            <ion-icon name="call"></ion-icon> <div className="txtphno">+91-98655 41789</div>
                         </div>
                         <div className="e-mail">
-                        <ion-icon name="mail-open"></ion-icon> <div>stayeasy@gmail.com</div>
+                            <ion-icon name="mail-open"></ion-icon> <div className="txtmail">stayeasy@gmail.com</div>
                         </div>
                     </div>
                     <div className="socialmedia">
@@ -34,7 +34,7 @@ const Header = () => {
                 </div>
                 <div className="main-header-area">
                     <div className="logo-sec">
-                        <img src="/images/logo.svg" alt="logo"/>
+                        <img src="/images/logo.svg" alt="logo" />
                     </div>
                     <div className="nav-sec">
                         <ul>
@@ -50,11 +50,31 @@ const Header = () => {
                             <li><NavLink to="/login">Login → </NavLink></li>
                         </ul>
                     </div>
+
+                    <div className="responsive-toggle">
+                        <input type="checkbox" id="check" className="toggle-input" />
+                        <label htmlFor="check" className="checkbtn">
+                            <ion-icon  name="reorder-three-outline"></ion-icon>
+                        </label>
+                        <div className="resp-nav-sec">
+                        <ul className="register-login-sec-2">
+                            <li><NavLink to="/register">Register</NavLink></li>
+                            <li><NavLink to="/login">Login</NavLink></li>
+                        </ul>
+                        <ul>
+                            <li><img src="/images/logo.svg" alt="logo" /></li>
+                            <li><NavLink to="/home">Home</NavLink></li>
+                            <li><NavLink to="/rooms">Rooms</NavLink></li>
+                            <li><NavLink to="/contacts">Contact</NavLink></li>
+                            <li><NavLink to="/about">About Us</NavLink></li>
+                        </ul>
+                    </div>
+                    </div>
+                    
                 </div>
             </div>
-
         </>
-    );
+    )
 };
 
 export default Header;
