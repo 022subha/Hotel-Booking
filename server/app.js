0b84c7beaa7dd67fcc638120e69f52760bf6f131
Subhajit <022subha@gmail.com>
2023-06-23 10:50:34 +0530

    header css modified

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ import fileUpload from "express-fileupload";
 const app = express();
 
 app.use(
-  cors({
+  cors(/* {
     origin: "https://stayeasy.vercel.app ",
-  })
+  } */)
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,9 @@ app.use(
   })
 );
 
+import paymentRoute from "./routes/paymnetRoute.js";
 import userRoute from "./routes/userRoute.js";
 app.use("/api/auth", userRoute);
+app.use("/api/payment", paymentRoute);
 
 export default app;
