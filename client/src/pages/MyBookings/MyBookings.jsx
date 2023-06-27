a3d6e1d0c1feb83cc352cb76e59147a0ebc255cc
Bijay kumar sah <vijaykumarsah293@gmail.com>
2023-06-27 11:43:57 +0530

    my-bokings page created

---
+++ client/src/pages/MyBookings/MyBookings.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '../../components/Card/Card'
+
+const MyBookings = () => {
+  return (
+    <div className="room-container">
+      {/* <div className="filter-container"></div> */}
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+
+)}
+
+export default MyBookings
