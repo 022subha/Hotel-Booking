641e1b586477032ca7042b07a9291aa1d4e04b47
Golu kumar <anubhavanandgolu@gmail.com>
2023-06-29 14:38:26 +0530

    dashboard

--- client/src/components/Dashboard/DashboardLayout.jsx
+++ client/src/components/Dashboard/DashboardLayout.jsx
@@ -3,73 +3,31 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import "./DashboardLayout.css";
+import DashboardNavbar from "./DashboardNavbar";
+import DashboardSidebar from "./DashbordSidebar";
 
-export default function DashboardLayout({ children }) {
+export default function Dashboard({ children }) {
   const location = useLocation();
   const [sideBarActive, setSideBarActive] = useState(false);
   const { user } = useSelector((state) => state.user);
 
-  const toggle = () => {
-    setSideBarActive(!sideBarActive);
+  const openSidebar = () => {
+    setSideBarActive(true);
+  };
+
+  const closeSidebar = () => {
+    setSideBarActive(false);
   };
   useEffect(() => {
     console.log(user);
   }, [user]);
   return (
-    <div className="dashboard-container">
-      <div className="main-container">
-        <div className={`sidebar-content ${sideBarActive ? "active" : ""}`}>
-          <div className="sidebar">
-            <div className="front">
-              <img src="/images/logo.svg" alt=""></img>
-            </div>
-
-            <div className="nav">
-              <Link to="/admin/dashboard">
-                <ion-icon name="grid"></ion-icon>
-                <h2>Dashboard</h2>
-              </Link>
-            </div>
-
-            <div className="nav">
-              <Link to="/admin/add-room">
-                <ion-icon name="add"></ion-icon>
-                <h2>Add Rooms</h2>
-              </Link>
-            </div>
-
-            <div className="nav">
-              <Link to="/admin/rooms">
-                <ion-icon name="home"></ion-icon>
-                <h2>View Rooms</h2>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="content">
-          <div className="header" onClick={() => toggle()}>
-            <ion-icon name="menu-outline"></ion-icon>
-            <div className="front">
-              <div className="dash">
-                {location.pathname.split("/")[2].toLocaleUpperCase()}
-              </div>
-            </div>
-            <div className="upper">
-              <div className="notification">
-                <ion-icon name="notifications"></ion-icon>
-              </div>
-              <div className="inner">
-                <Avatar
-                  src={user?.avatar}
-                  icon={<ion-icon name="notifications"></ion-icon>}
-                  size={50}
-                />
-                <div className="name">{user?.name.split(" ")[0]}</div>
-              </div>
-            </div>
-          </div>
-          <div className="body">{children}</div>
-        </div>
+    <div className="dashboardlayout-container">
+      {sideBarActive && <div className="overlay" onClick={closeSidebar} />}
+      <DashboardSidebar sideBarActive={sideBarActive} />
+      <div className="main-content">
+        <DashboardNavbar openSidebar={openSidebar} />
+        <div className="body">{children}</div>
       </div>
     </div>
   );
